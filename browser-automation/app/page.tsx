a7c36690fbfa498ef113b1085c62529c370ed569
Oliver Metz <softwaredevdad@gmail.com>
2026-09-03 21:03:44 +0200

    feat: add create organization modal/multi organization abilities.

--- browser-automation/app/page.tsx
+++ browser-automation/app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 
-import { UserButton } from "@clerk/nextjs"
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
 
 export default function Page() {
   return (
-    <div className="min-h-svh p-6">
+    // <div className="min-h-svh p-6">
+    <div className="flex flex-col items-start gap-4">
       <UserButton />
+      <OrganizationSwitcher />
     </div>
   )
 }
